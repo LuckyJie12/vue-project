ad88159e9494878a47475ade8841f1cae7782e55
Shmily <2996985766@qq.com>
2025-10-24 17:41:49 +0800

    更新重定向到console

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ConsoleView from '@/views/ConsoleView.vue'
-import AnalyticsView from '@/views/AnalyticsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ConsoleView from "@/views/ConsoleView.vue";
+import AnalyticsView from "@/views/AnalyticsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/console',
-      name: 'console',
+      path: "/",
+      redirect: "/console",
+    },
+    {
+      path: "/console",
+      name: "console",
       component: ConsoleView,
     },
     {
-      path: '/analytics',
-      name: 'home',
+      path: "/analytics",
+      name: "home",
       component: AnalyticsView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
